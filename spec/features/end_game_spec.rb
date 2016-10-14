560fb91942baed68b15f1bb12375c8664d89e792
lcbeh <lichien2010@yahoo.co.uk>
2016-10-14 15:12:42 +0100

    Add check_winner feature

---
+++ spec/features/end_game_spec.rb
@@ -0,0 +1,12 @@
+require "./app"
+
+feature  "Game Over" do
+
+  scenario "when a player reaches 0HP" do
+    sign_in_and_play
+    until $game.winner != nil
+      click_button "Attack"
+    end
+    expect(page).to have_text("#{$game.winner.name} won!")
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,7 @@ require 'player'
 
 describe Player do
   subject(:player) {described_class.new}
-  
+
   it "returns the name of the player 1" do
     player1 = Player.new("Bob")
     expect(player1.name).to eq("Bob")
@@ -12,4 +12,5 @@ describe Player do
     player2 = Player.new("Steve")
     expect(player2.name).to eq("Steve")
   end
+
 end
